19b8b1488a2db5cd33ffdd973d23805ab493c8c2
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-21 23:11:34 +0200

    исправил обьявление переменных, убрал лишнее

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // 1) Создаю переменную num со значением 266219 (тип данных число)
-const num = 266219
+let num = 266219,
+    numSelfMulti = 1;
 
 // 2) Вывожу в консоль произведение цифр этого числа в переменной num
-numStr = num.toString();
-numSelfMulti = 1;
-for (let i = 0; i < numStr.length; i++){
-    numSelfMulti *= parseInt(numStr[i])
+num = num.toString();
+for (let i = 0; i < num.length; i++){
+    numSelfMulti *= parseInt(num[i])
 };
 
 console.log(numSelfMulti);
 
 // 3) Полученный результат возвожу в куб
-numSelfMultiPow = numSelfMulti ** 3;
+let numSelfMultiPow = numSelfMulti ** 3;
 
 // 4) Вывожу на экран первые 2 цифры полученного числа
 numSelfMultiPow = numSelfMultiPow.toString();
