f12f89d19aed44ebcc1bab3ee01b3c9f74079878
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-27 19:24:37 +0200

    tasl 7 ready

--- arr/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const arr = ['4567', '7658', '3945', '2943', '7403', '4444', '8572'];
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] == 4 || arr[i][0] == 2) {
-        console.log(arr[i]);
-    }
-}
---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "<i>Суббота</i>", "<i>Воскресенье</i>"];
+    
+const todayIndex = (6 + new Date().getDay()) % 7;
+days[todayIndex] = `<b>${ days[todayIndex] }</b>`;
+
+const nowDate = new Date().getDay();
+
+document.body.insertAdjacentHTML(
+    "beforeEnd", 
+    `
+    <h2>Задание</h2>
+    <h3>Создать массив week и записать в него дни недели в виде строк</h3>
+    <ul>
+        <li>Вывести на экран все дни недели</li>
+        <li>Каждый из них с новой строчки</li>
+        <li>Выходные дни - курсивом</li>
+        <li>Текущий день - жирным шрифтом(использовать объект даты)</li>
+    </ul>
+    <h2><i>Решение</i></h2>    
+    ${days.join("<br>")}
+    `);
