64975a62047ed062d89eb16bb18fbf496fa286d3
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-12-14 20:38:56 +0200

    throttling or debouncing

--- script/main.js
+++ script/main.js
@@ -13,4 +13,18 @@ window.addEventListener('DOMContentLoaded', () => {
     const userInput = createElement('input', 'text-input', 'user-text'),
         textBlock = createElement('p');
     userInput.setAttribute('type', 'text');
+    userInput.setAttribute('placeholder', 'Введите текст...')
+
+    function debounce (handler, timeStop) {
+        return function (args) {
+            let prevCall = this.lastCall;
+            this.lastCall = Date.now();
+            if (prevCall && (this.lastCall - prevCall) <= timeStop) {
+                clearTimeout(this.lastCallTimer);
+            }
+            this.lastCallTimer = setTimeout(() => handler(args), timeStop);
+        };
+    }
+    let inputUserText = (args) => textBlock.textContent = userInput.value;
+    userInput.addEventListener('keyup', debounce(inputUserText, 300));
 });
